53f2a9fd924a24b2aa07833d341be80f97d323b1
Luciana Pereira <luci.lv14@gmail.com>
2019-08-23 12:07:57 -0300

    alterações do desafio

---
+++ admissionProjectWFS01/app.js
@@ -0,0 +1,8 @@
+<script type ="text/JavaScript">
+var usuario = alert("O email" + usuario + "foi enviado");
+
+if (usuario == null || usuario == "") {
+   text = "O email" + usuario + "foi enviado";
+}     
+
+</script>
